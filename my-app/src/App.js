7aec761abbd1d4475faecfa6fa891db91b6d7c59
Simone <simonestormbk@gmail.com>
2024-04-19 08:35:10 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Main from './Components/Main.jsx';
+import './Components/style.css'
+
+function App() {
+  return (
+    <>
+   <Main/>
+   </>
+  );
+}
+
+export default Main;
